7a14f447807f0838cd9b4d92100d7683fd8f6bc5
balasaheb <balasaheb@DARSHANA>
2022-12-01 17:58:49 +0530

    thursady activity

--- activity3/src/que1/reverse.java
+++ activity3/src/que1/reverse.java
@@ -8,17 +8,51 @@ import java.util.Scanner;
 	 {
 		 public void reverse()
 		 {
-			 Scanner sc = new Scanner(systrm.in);
-			 System.out.printIn("\nreverse the didgit");
-			 System.out.printIn("\n Enter the which no u want to reverse");
-			 int num = sc.nextInt();
-			 int rev=0;
-			 while(num ! = 0)
-			 {
-				 int remainder = mun % 10;
-				 rev = rev * 10 + remainder;
-				 num = num/10;
-				 
-			 }
-		 }
-	 }
+			
+			 
+			   
+			    public static void main(String[] args)
+			    {
+			        
+			        int num, rem;
+			 
+			        
+			        int rev = 0, sum = 0;
+			 
+			        
+			        System.out.println("Enter the number: 25 ");
+			 
+			        num = 25;
+			 
+			        
+			        do {
+			 
+			            
+			            rem = num % 10;
+			 
+			            // Step2: Reverse the number
+			            rev = rev * 10 + rem;
+			 
+			            // Sum of the digits of number
+			            sum = sum + rem;
+			 
+			            // Step3: Dividing number by 10 to lose last
+			            // digit
+			            num = num / 10;
+			        }
+			 
+			        // Condition check
+			        // Remember: By this time 1 iteration is over even
+			        // if conditions false
+			        while (num > 0);
+			 
+			        // Printing the reverse number
+			        System.out.println("Reverse of given number: "
+			                           + rev);
+			 
+			        // Summing up digits of number as shown in above
+			        // steps
+			        System.out.println("Sum of digits of given number: "
+			                           + sum);
+			    }
+			}
